09ddbf0521da1464f00c1645c3d41ff8a47b7231
PJ Janse van Rensburg <petrus.jvrensburg@gmail.com>
2018-11-16 08:04:27 +0200

    Generate data for testing cross validation.

--- tests/test_cross_validation.py
+++ tests/test_cross_validation.py
@@ -1,6 +1,8 @@
 import pytest
+import numpy as np
 
 from lightfm.cross_validation import random_train_test_split
+from lightfm.data import Dataset
 from lightfm.datasets import fetch_movielens
 
 
@@ -12,13 +14,24 @@ def _assert_disjoint(x, y):
     for (i, j) in zip(y.row, y.col):
         assert x[i, j] == 0.0
 
+def _fake_data(n):
+    users = np.random.choice(10000, (n, 1))
+    items = np.random.choice(10000, (n, 1))
+    weight = np.random.rand(n,1)
+    return np.concatenate((users, items, weight), axis=1)
 
 @pytest.mark.parametrize("test_percentage", [0.2, 0.5, 0.7])
 def test_random_train_test_split(test_percentage):
 
-    data = fetch_movielens()["train"]
+    data = _fake_data(1000)
 
-    train, test = random_train_test_split(data, test_percentage=test_percentage)
+    # Use Dataset to prep your interactions and weights.
+    dataset = Dataset()
 
-    assert test.nnz / float(data.nnz) == test_percentage
+    dataset.fit(users=np.unique(data[:, 0]), items=np.unique(data[:, 1]))
+    interactions, sample_weight = dataset.build_interactions((i[0], i[1], i[2]) for i in data)
+
+    train, test = random_train_test_split(interactions, test_percentage=test_percentage)
+
+    assert test.nnz / float(interactions.nnz) == test_percentage
     _assert_disjoint(train, test)
